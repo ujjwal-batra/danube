fb89b4ff4ec2b10d63d29a2fe69e2bd351387061
ujjwal <ujjwal24batra@gmail.com>
2020-06-16 11:59:21 +0530

    minor changes

---
+++ src/map.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidWpqd2FsLWJhdHJhIiwiYSI6ImNrYXBvcWpidTBmZDgydXFuNmw2bGV5NjUifQ.cj_kQ8JKiMBYpyWacEiB6w';
